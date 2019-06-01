86c9d4d41485dd5e137425e355154985188fbffd
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-06-01 15:02:28 -0400

    finished

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/24/18.
  */
@@ -11,7 +14,18 @@ public class PersonHandler {
     }
 
     public String whileLoop() {
-        String result = "";
+        Integer counter = 0;
+        StringBuilder result = new StringBuilder ();
+        while(counter < personArray.length){
+            Person currentPerson = personArray[counter];
+            result.append(currentPerson.toString());
+            counter++;
+
+
+
+        }
+            return result.toString();
+        }
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
@@ -21,24 +35,31 @@ public class PersonHandler {
                 // append `stringRepresentation` to `result` variable
 
             // end loop
-        return result;
-    }
 
 
 
     public String forLoop() {
-        String result = "";
+        int initValue;
+        int termCond;
+        int i;
+        Integer counter = 0;
+        StringBuilder result = new StringBuilder ();
         // identify initial value
         // identify terminal condition
         // identify increment
+        for(i = 0; i <= counter; i++) {
+            counter++;
+            String currentPerson = counter.toString();
+            result.append(currentPerson);
+            result.toString();
 
-        // use the above clauses to declare for-loop signature
+            // use the above clauses to declare for-loop signature
             // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
             // end loop
-
+        }
         return result;
     }
 
